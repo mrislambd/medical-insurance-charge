79a4a57104008a1727020df9959a2c559e32fa8f
Rafiq Islam <mi21b@fsu.edu>
2024-09-16 14:06:29 -0400

    cleaned

--- feature_engineering.py
+++ feature_engineering.py
@@ -1,9 +1,6 @@
-# feature_engineering.py
-
 import pandas as pd
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.preprocessing import OneHotEncoder, LabelEncoder
-import numpy as np
 
 
 class FeatureEngineering(BaseEstimator, TransformerMixin):
