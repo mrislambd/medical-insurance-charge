99303576b1fe6b272028ebe0cbb955251573e984
Rafiq Islam <mi21b@fsu.edu>
2024-09-17 02:33:07 -0400

    sidebar

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 import streamlit as st
 from predict_page import prediction
 from explore_page import show_explore_page
-from streamlit_navigation_bar import st_navbar
 
-page = st_navbar(["Predict", "Explore"])
+page = st.sidebar.selectbox("Explore Or Predict", ("Predict", "Explore"))
 
 if page == 'Predict':
     prediction()
